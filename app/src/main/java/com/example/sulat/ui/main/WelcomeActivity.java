ada6fdcb5252c05b7e9dd72fb51299164fdb695d
BanCoca <harveycoca0219@gmail.com>
2024-05-06 19:13:27 +0800

    ADD BUTTONS FUNCTIONALITIES

--- app/src/main/java/com/example/sulat/ui/main/WelcomeActivity.java
+++ app/src/main/java/com/example/sulat/ui/main/WelcomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.sulat.ui.main;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +14,10 @@ import androidx.core.splashscreen.SplashScreen;
 import com.example.sulat.R;
 
 public class WelcomeActivity extends AppCompatActivity {
-
+    Button btnLogin, btnSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //SplashScreen.installSplashScreen(this);
-
+        SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome);
@@ -24,6 +25,15 @@ public class WelcomeActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //BUTTONS FOR LOGIN AND SIGNUP
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
         });
     }
 }
